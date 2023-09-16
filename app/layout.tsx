b169768b2a4cea0ba04ebefd3ed02a6dcdbe97d2
Luca Bruno <lucabru@gmail.com>
2023-09-16 18:39:52 +0200

    fix: metadata document title changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Casino Lobby",
+  title: "Casino Portal",
   description: "Browse through the hottest games through the Casino Lobby!"
 }
 
--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,12 +1,16 @@
 import MainContent from "@/components/MainContent/MainContent"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Casino Portal | Lobby",
+  description: "Browse through the hottest games through the Casino Lobby!"
+}
 
 export default function Home() {
 
   return (
-    <main>
-      <div className="grid grid-cols-3 h-[calc(100vh-48px)]">
-        <MainContent />
-      </div>
+    <main className="grid grid-cols-3 h-[calc(100vh-48px)]">
+      <MainContent />
     </main>
   )
 }
