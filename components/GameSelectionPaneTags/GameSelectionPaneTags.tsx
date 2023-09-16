e8f59554efff58b96bb1e5f25006962a48409e8d
Luca Bruno <lucabru@gmail.com>
2023-09-16 18:58:32 +0200

    fix: minor styling

--- components/GameSelectionPaneTags/GameSelectionPaneTags.tsx
+++ components/GameSelectionPaneTags/GameSelectionPaneTags.tsx
@@ -3,7 +3,7 @@ import GameTags from "../GameTags"
 import GameSelectionPaneTagsTypes from "./types/GameSelectionPaneTags.interface"
 
 const GameSelectionPaneTags: React.FC<GameSelectionPaneTagsTypes> = ({ categoryTags, featureTags, themeTags }) => (
-    <div className="absolute bottom-3 w-[80%]">
+    <div className="mt-6 w-[80%]">
         {categoryTags && <GameTags tags={categoryTags} />}
         {featureTags && <GameTags tags={featureTags} />}
         {themeTags && <GameTags tags={themeTags} />}
