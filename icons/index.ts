26416ad1c39458aec0ca92cfc70e84c6925befc2
Luca Bruno <lucabru@gmail.com>
2023-09-17 10:24:32 +0200

    fix: Font Awesome package + individual imports

---
+++ icons/index.ts
@@ -0,0 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core"
+import {
+  fas,
+  faChevronDown
+} from "@fortawesome/free-solid-svg-icons"
+
+library.add(
+  fas,
+  faChevronDown
+)
