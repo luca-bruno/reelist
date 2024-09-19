19f40da8cc485e3200392b5a69bae56ac121abe7
Luca Bruno <lucabru@gmail.com>
2024-09-19 17:59:48 +0200

    fix: scroll bar restyling + configs

--- json/data.interface.ts
+++ json/data.interface.ts
@@ -31,4 +31,31 @@ interface movieFullTypes {
   Production: string
 }
 
-export type { movieSummaryTypes, ratingsTypes, movieFullTypes }
+
+
+interface Result {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+interface Root {
+  page: number
+  results: Result[]
+  total_pages: number
+  total_results: number
+}
+
+
+export type { movieSummaryTypes, ratingsTypes, movieFullTypes, Result, Root }
