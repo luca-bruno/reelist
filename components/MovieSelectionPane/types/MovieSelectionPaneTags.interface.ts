7d35d4471bb76222536030b50d51d6ba8b5d4d8a
Luca Bruno <lucabru@gmail.com>
2024-09-29 15:33:39 +0200

    fix: removed old tags component

--- components/MovieSelectionPane/types/MovieSelectionPaneTags.interface.ts
+++
@@ -1,9 +0,0 @@
-import { dataSubtypes } from "@/types/movies.interface"
-
-interface MovieSelectionPaneTagsTypes {
-  categoryTags?: dataSubtypes[]
-  featureTags?: dataSubtypes[]
-  themeTags?: dataSubtypes[]
-}
-
-export default MovieSelectionPaneTagsTypes
