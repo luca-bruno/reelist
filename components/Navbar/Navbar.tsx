599f8e08dd5f838bbfe3070f21e41b753fec219b
Luca Bruno <lucabru@gmail.com>
2024-10-03 18:29:21 +0200

    feat: country selector in Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line camelcase
 import { Oleo_Script } from "next/font/google"
 import NavbarButton from "./NavbarButton"
+import CountrySelector from "../CountrySelector"
 
 const oleoScript = Oleo_Script({
   subsets: ["latin"],
@@ -9,9 +10,12 @@ const oleoScript = Oleo_Script({
 
 const Navbar = () => (
   <header className={`flex justify-between h-12 ${oleoScript.className}`}>
-    <p className="text-4xl px-4 mt-2 flex justify-center items-center select-none text-accent-500">
+    <p className="text-4xl px-3 mt-2 flex justify-center items-center select-none text-accent-500">
       <NavbarButton {...{ label: "Reelist", slug: "/" }} />
     </p>
+
+    <CountrySelector />
+
     {/* <NavbarButtons /> */}
   </header>
 )
