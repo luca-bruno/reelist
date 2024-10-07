4a0fdba5551303dcc93b10aae467ccb600fd86ec
Luca Bruno <lucabru@gmail.com>
2024-10-07 02:36:25 +0200

    feat: Results page navigation buttons (w/o functionality)

--- components/Browse/Browse.tsx
+++ components/Browse/Browse.tsx
@@ -1,37 +1,11 @@
 "use client"
 
 import { FC, useEffect, useState } from "react"
-// import { faList } from "@fortawesome/free-solid-svg-icons/faList"
-// import { faGrip } from "@fortawesome/free-solid-svg-icons/faGrip"
-// import { faFilter } from "@fortawesome/free-solid-svg-icons/faFilter"
 import { movieTypes } from "@/types/movie.interface"
 import { HEADERS_ALLOW_ORIGIN, IS_BROWSER } from "@/constants"
 import Search from "../Search"
-// import Filter from "../Filter"
-// import ClearFiltersButton from "../ClearFiltersButton"
-// import ToggleButton from "../ToggleButton"
 import MovieCardList from "../MovieCardList"
 import MovieSelectionPane from "../MovieSelectionPane"
-
-// TODO: maybe use as fallback? needs testing
-// const scoreMovies = (movies, currentMovie) => {
-//   const scoredMovies = movies.map(movie => {
-//       let score = 0
-
-//       // Count genre matches (assuming you have genre IDs)
-//       const genreMatches = movie.genre_ids.filter(genre => currentMovie.genres.includes(genre))
-//       score += genreMatches.length * 2 // Higher weight for genre matches
-
-//       // Count cast matches (assuming you have cast member IDs)
-//       const castMatches = movie.cast?.filter(actor => currentMovie.cast.includes(actor))
-//       score += castMatches?.length // Lower weight for cast matches
-
-//       return { movie, score }
-//   })
-
-//   // Sort by score
-//   return scoredMovies.sort((a, b) => b.score - a.score).map(item => item.movie)
-// }
 
 const Browse: FC<{ params?: { id?: string; key?: string } }> = ({ params }) => {
   const { id, key } = params || {}
@@ -47,29 +21,6 @@ const Browse: FC<{ params?: { id?: string; key?: string } }> = ({ params }) => {
     localStorage.setItem("has-user-previously-visited", "true")
   }, [])
 
-  // const [isDisplayingGridView, setIsDisplayingGridView] = useState(true)
-  // const [isDisplayingFilters, setIsDisplayingFilters] = useState(true)
-  // const [selectedProviderFilters, setSelectedProviderFilters] = useState<string[]>([])
-  // const [selectedCategoryFilters, setSelectedCategoryFilters] = useState<string[]>([])
-  // const [selectedFeatureFilters, setSelectedFeatureFilters] = useState<string[]>([])
-  // const [selectedThemeFilters, setSelectedThemeFilters] = useState<string[]>([])
-
-  // const haveFiltersBeenSelected =
-  //   selectedProviderFilters.length > 0 ||
-  //   selectedCategoryFilters.length > 0 ||
-  //   selectedFeatureFilters.length > 0 ||
-  //   selectedThemeFilters.length > 0
-
-  // const clearFilters = () => {
-  //   setSelectedProviderFilters([])
-  //   setSelectedCategoryFilters([])
-  //   setSelectedFeatureFilters([])
-  //   setSelectedThemeFilters([])
-  // }
-
-  const alignmentStyles = "flex justify-start items-start"
-  // const filterWrapperStyles =
-  //   "text-black laptopM:[&>*:not(:first-child)]:mx-2 mobileXL:[&>*:not(:first-child)]:mx-0 [&>*:not(:first-child)]:mx-2 laptopM:mr-1 mx-0 mr-3"
 
   useEffect(() => {
     async function fetchMovieById() {
@@ -125,25 +76,28 @@ const Browse: FC<{ params?: { id?: string; key?: string } }> = ({ params }) => {
   }, [defaultMovieDetails, formattedCastMembers, query])
 
   return (
-    <main className="grid grid-flow-row grid-rows-2 mobileXL:grid-rows-none mobileXL:grid-cols-3 h-[calc(100vh-48px)]">
-      <div className="rounded-xl m-3 mobileL:overflow-x overflow-x-none bg-neutral-500">
+    <main className="grid grid-flow-row grid-rows-2 mobileXL:grid-rows-none mobileXL:grid-cols-3">
+      <div className="rounded-xl m-3 mobileL:overflow-x overflow-x-none bg-neutral-500 h-[calc(100vh-75px)] flex flex-col">
         <div className={`${alignmentStyles} ml-3 mt-3 mr-2`}>
           <Search setQuery={setQuery} />
 
-          {/* <div className="m-auto mobileL:mr-1 mobileXL:h-10 h-max flex ml-0">
-            <ToggleButton
-              state={isDisplayingGridView}
-              stateSetter={setIsDisplayingGridView}
-              onIcon={faList}
-              offIcon={faGrip}
-            />
-
-            <ToggleButton
-              state={isDisplayingFilters}
-              stateSetter={setIsDisplayingFilters}
-              onIcon={faFilter}
-            />
-          </div> */}
+          <div className="m-auto mobileL:mr-1 mobileXL:h-10 h-max flex ml-0">
+            {/* <ToggleButton state={isDisplayingGridView} stateSetter={setIsDisplayingGridView} onIcon={faList} offIcon={faGrip} /> */}
+            <button
+              type="button"
+              className={`w-10 ${buttonStyles}`}
+              // onClick={() => stateSetter(prev => !prev)}
+            >
+              <FontAwesomeIcon className="h-5 w-5 text-white flex m-auto" icon={faArrowLeft} aria-hidden="true" />
+            </button>
+            <button
+              type="button"
+              className={`w-10 ${buttonStyles}`}
+              // onClick={() => stateSetter(prev => !prev)}
+            >
+              <FontAwesomeIcon className="h-5 w-5 text-white flex m-auto" icon={faArrowRight} aria-hidden="true" />
+            </button>
+          </div>
         </div>
 
         {/* <div
@@ -188,14 +142,9 @@ const Browse: FC<{ params?: { id?: string; key?: string } }> = ({ params }) => {
           {...{
             setSelectedMovieId,
             selectedMovieId,
-            query,
             // genres,
             movies,
             isDisplayingGridView: true
-            //   selectedProviderFilters,
-            //   selectedCategoryFilters,
-            //   selectedFeatureFilters,
-            //   selectedThemeFilters
           }}
         />
       </div>
