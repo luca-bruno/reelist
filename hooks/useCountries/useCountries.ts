adb56ce8f874076541ad51c55a3b05cd9ab7dcad
Luca Bruno <lucabru@gmail.com>
2024-10-09 04:05:38 +0200

    fix: note

--- hooks/useCountries/useCountries.ts
+++ hooks/useCountries/useCountries.ts
@@ -1,6 +1,7 @@
 import useSWR from "swr"
 import { SWR_FETCHER } from "@/constants"
 
+// TODO: undo this bc not to be fetched on client
 // NOTE: For cached calls to route handler
 const useCountries = (skipCondition: boolean) => {
   const { data, error, isLoading } = useSWR(!skipCondition ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/countries` : null, SWR_FETCHER, {
