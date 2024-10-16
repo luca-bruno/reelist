227b02d777dc98be6094fd19746ce643085c6242
Luca Bruno <lucabru@gmail.com>
2024-10-16 22:54:27 +0200

    feat: skeleton loading imgs on cast/crew

--- components/MovieSelectionPane/MovieSelectionPaneCastCrewDetails.tsx
+++ components/MovieSelectionPane/MovieSelectionPaneCastCrewDetails.tsx
@@ -1,9 +1,6 @@
 import { FC } from "react"
-import Image from "next/image"
-import { TMDB_IMAGE_PATH } from "@/constants"
-import fallbackPlaceholderUser from "@/public/fallbackPlaceholderUser.jpg"
-import Link from "next/link"
 import MovieSelectionPaneCastCrewDetailsTypes from "./types/MovieSelectionPaneCastCrewDetails.interface"
+import MovieSelectionPaneCastCrewDetailsItem from "./MovieSelectionPaneCastCrewDetailsItem"
 
 const MovieSelectionPaneCastCrewDetails: FC<MovieSelectionPaneCastCrewDetailsTypes> = ({ cast, crew }) => {
   const directors = crew
@@ -38,58 +35,16 @@ const MovieSelectionPaneCastCrewDetails: FC<MovieSelectionPaneCastCrewDetailsTyp
     <>
       <h2 className="text-lg font-semibold mb-1">Director(s)</h2>
       <div className="grid grid-cols-2 gap-x-4">
-        {directors?.map(({ name, profilePath, id }) => (
-          <Link key={id} href={`/director/${id}`}>
-            <div className="mb-3 text-sm hover:bg-[gray] hover:bg-opacity-70 rounded-xl pr-2 py-1">
-              <div className="grid gap-1">
-                <div className="flex justify-between items-center h-[45px] gap-x-4 pl-2 rounded transition duration-200 ease-in-out">
-                  <div className="flex items-center">
-                    <Image
-                      unoptimized
-                      className="rounded-xl select-none slide_fade_from_left"
-                      src={profilePath ? `${TMDB_IMAGE_PATH}${profilePath}` : fallbackPlaceholderUser}
-                      alt={name}
-                      width={30}
-                      height={30}
-                      draggable={false}
-                    />
-                    <p className="font-semibold pl-2">{name}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        ))}
+        {directors?.map(({ name, profilePath, id }) => <MovieSelectionPaneCastCrewDetailsItem {...{ name, profilePath, id }} />)}
       </div>
       <h2 className="text-lg font-semibold mb-1">Cast</h2>
       <div className="grid grid-cols-2 gap-x-4">
-        {actors?.map(({ name, character, profilePath, id }) => (
-          <Link key={id} href={`/actor/${id}`}>
-            <div className="mb-3 text-sm hover:bg-[gray] hover:bg-opacity-70 rounded-xl pr-2 py-1">
-              <div className="grid gap-1">
-                <div className="flex justify-between items-center h-[45px] gap-x-4 pl-2 rounded transition duration-200 ease-in-out">
-                  <div className="flex items-center">
-                    <Image
-                      unoptimized
-                      className="rounded-xl select-none slide_fade_from_left"
-                      src={profilePath ? `${TMDB_IMAGE_PATH}${profilePath}` : fallbackPlaceholderUser}
-                      alt={`${name}`}
-                      width={30}
-                      height={30}
-                      draggable={false}
-                    />
-                    <p className="font-semibold pl-2">{`${name}`}</p>
-                  </div>
-                  <div>{character && <p className="text-end">{character}</p>}</div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        ))}
+        {actors?.map(({ name, character, profilePath, id }) => <MovieSelectionPaneCastCrewDetailsItem {...{ name, profilePath, id, character }} />)}
       </div>
 
       <div>
         {result?.map(item => (
+          // TODO: maybe integrate into MovieSelectionPaneCastCrewDetailsItem too?
           <div key={item.department} className="mb-3">
             <h2 className="text-lg font-semibold mb-1">{item.department}</h2>
             <div className="grid grid-cols-2 gap-x-4 text-sm">
---
+++ components/MovieSelectionPane/MovieSelectionPaneCastCrewDetailsItem.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import { TMDB_IMAGE_PATH } from "@/constants"
+import fallbackPlaceholderUser from "@/public/fallbackPlaceholderUser.jpg"
+import useImage from "@/hooks/useImage/useImage"
+import Skeleton from "react-loading-skeleton"
+
+interface MovieSelectionPaneCastCrewDetailsItemTypes {
+  id: number
+  name: string
+  profilePath: string
+  character?: string
+}
+
+const MovieSelectionPaneCastCrewDetailsItem: FC<MovieSelectionPaneCastCrewDetailsItemTypes> = ({ id, name, profilePath, character }) => {
+  const { hasImageLoaded, setHasImageLoaded } = useImage()
+
+  return (
+    <Link key={id} href={character ? `/actor/${id}` : `/director/${id}`}>
+      <div className="mb-3 text-sm hover:bg-[gray] hover:bg-opacity-70 rounded-xl pr-2 py-1">
+        <div className="grid gap-1">
+          <div className="flex justify-between items-center h-[45px] gap-x-4 pl-2 rounded transition duration-200 ease-in-out">
+            <div className="flex items-center">
+              {!hasImageLoaded && (
+                <Skeleton height={45} width={30} enableAnimation className="absolute top-[-1px] left-0 !rounded-xl" highlightColor="#d6d6d6" />
+              )}
+              <Image
+                unoptimized
+                className={`rounded-xl ${hasImageLoaded ? "opacity-100" : "opacity-0"} select-none`}
+                onLoadingComplete={() => setHasImageLoaded(true)}
+                src={profilePath ? `${TMDB_IMAGE_PATH}${profilePath}` : fallbackPlaceholderUser}
+                alt={name}
+                width={30}
+                height={30}
+                draggable={false}
+              />
+              <p className="font-semibold pl-2">{name}</p>
+            </div>
+            {character && (
+              <div>
+                <p className="text-end">{character}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default MovieSelectionPaneCastCrewDetailsItem
