a50c980eb5edb31e8b55744691c3b2d5163de0c7
Luca Bruno <lucabru@gmail.com>
2024-10-24 02:50:06 +0200

    fix: x-forwarded-for

--- app/api/locale/route.ts
+++ app/api/locale/route.ts
@@ -1,20 +1,21 @@
 /* eslint-disable import/prefer-default-export */
-import fetchMovie from "@/services/fetchMovie/fetchMovie"
 
 // NOTE: For fetching from client components
 export async function GET(request: Request) {
+  const clientIpAddress = request.headers.get("x-forwarded-for")
+
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.TMDB_BEARER_TOKEN}`
+      "Content-Type": "application/json"
+      // Authorization: `Bearer ${process.env.TMDB_BEARER_TOKEN}`
     }
   }
 
   try {
-    const response = await fetch("https://ipapi.co/json/", options)
+    const response = await fetch(`https://ipapi.co/${clientIpAddress}/json/`, options)
     const data = (await response.json()) as any
 
     return new Response(JSON.stringify(data))
